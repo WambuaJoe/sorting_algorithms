1187fe0b324e02851d55ee0a95878ecf17d561cf
WambuaJoe <kaytujohn254@gmail.com>
2023-10-29 16:35:49 +0300

    use cocktail shaker sort algo to sort a doubly linked list of integers

--- sort.h
+++ sort.h
@@ -1,9 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
-/* Libraries used */
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,23 +13,34 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
-
-/* Function prototypes */
-void swap(int *arr, int idx1, int idx2);
-listint_t *create_listint(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int partition(int *array, int first, int last, size_t size);
+void qs(int *array, int first, int last, size_t size);
 void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size)
 
-#endif /*SORT_H*/
 
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t **left, listint_t **right);
+void print_left_right(int *array, int size, int first, int mid);
+void merge(int *array, int size, int *cpy, int first, int mid);
+void mergeSort(int *cpy, int first, int size, int *array);
+void copy_array(int *arr, int *cpy, int size);
+
+#endif /* SORT_H */
